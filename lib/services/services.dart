6a685c6fe952508eb1a1de921cc38e855ea07502
Boomexe <aptenathan@gmail.com>
2024-09-25 23:47:28 -0700

    commented out services.dart to disambiguate

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,75 +1,75 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:emergency_allergy_app/models/allergy.dart';
-import 'package:emergency_allergy_app/models/medication.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'package:emergency_allergy_app/models/allergy.dart';
+// import 'package:emergency_allergy_app/models/medication.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class Services {
-  static FlutterSecureStorage storage = const FlutterSecureStorage();
+// class Services {
+//   static FlutterSecureStorage storage = const FlutterSecureStorage();
 
-  static Future<List<Allergy>> loadAllergies() async {
-    Future<String?> allergiesString = readData('allergies');
+//   static Future<List<Allergy>> loadAllergies() async {
+//     Future<String?> allergiesString = readData('allergies');
 
-    allergiesString.then((value) {
-      List<Allergy> allergies =
-          Allergy.fromJsonList(jsonDecode(value!)['allergies']);
+//     allergiesString.then((value) {
+//       List<Allergy> allergies =
+//           Allergy.fromJsonList(jsonDecode(value!)['allergies']);
 
-      return allergies;
-    });
+//       return allergies;
+//     });
 
-    return [];
-  }
+//     return [];
+//   }
 
-  static void saveAllergy(allergy) async {
-    Future<List<Allergy>> allergies = loadAllergies();
+//   static void saveAllergy(allergy) async {
+//     Future<List<Allergy>> allergies = loadAllergies();
 
-    allergies.then((value) {
-      value.add(allergy);
+//     allergies.then((value) {
+//       value.add(allergy);
 
-      saveData('allergies', jsonEncode({'allergies': value}));
-    });
-  }
+//       saveData('allergies', jsonEncode({'allergies': value}));
+//     });
+//   }
 
-  static Future<List<Medication>> loadMedications() async {
-    String? medicationsString = await readData('medications');
+//   static Future<List<Medication>> loadMedications() async {
+//     String? medicationsString = await readData('medications');
 
-    if (medicationsString == null) {
-      return [];
-    }
+//     if (medicationsString == null) {
+//       return [];
+//     }
 
 
 
-    List<Medication> medications =
-        Medication.fromJsonList(jsonDecode(medicationsString)['medications']);
+//     List<Medication> medications =
+//         Medication.fromJsonList(jsonDecode(medicationsString)['medications']);
 
-    return medications;
-  }
+//     return medications;
+//   }
 
-  static void saveMedication(medication) async {
-    List<Medication> medications = await loadMedications();
+//   static void saveMedication(medication) async {
+//     List<Medication> medications = await loadMedications();
 
-    medications.add(medication);
-    saveData('medications', jsonEncode(Medication.toJsonList(medications)));
-  }
+//     medications.add(medication);
+//     saveData('medications', jsonEncode(Medication.toJsonList(medications)));
+//   }
 
-  static Future<List<Medication>> saveAndReturnMedications(medication) async {
-    List<Medication> medications = await loadMedications();
+//   static Future<List<Medication>> saveAndReturnMedications(medication) async {
+//     List<Medication> medications = await loadMedications();
 
-    medications.add(medication);
-    saveData('medications', jsonEncode(Medication.toJsonList(medications)));
-    return medications;
-  }
+//     medications.add(medication);
+//     saveData('medications', jsonEncode(Medication.toJsonList(medications)));
+//     return medications;
+//   }
 
-  static void saveData(key, value) async {
-    await storage.write(key: key, value: value);
-  }
+//   static void saveData(key, value) async {
+//     await storage.write(key: key, value: value);
+//   }
 
-  static Future<String?> readData(key) async {
-    // print(await storage.read(key: key));
-    return await storage.read(key: key);
-  }
+//   static Future<String?> readData(key) async {
+//     // print(await storage.read(key: key));
+//     return await storage.read(key: key);
+//   }
 
-  static void clearData() async {
-    await storage.deleteAll();
-  }
-}
+//   static void clearData() async {
+//     await storage.deleteAll();
+//   }
+// }
